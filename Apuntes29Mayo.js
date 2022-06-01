52972e2b45079c632bd7489d3bf4f6c10c4cba86
V3RNE42 <vatuviii@hotmail.com>
2022-06-01 18:18:36 +0200

    subida inicial: 1 Junio

---
+++ Apuntes29Mayo.js
@@ -0,0 +1,160 @@
+// Francesc Adrià Perelló18:03
+// Hola a todos!
+// Javier Guerra18:03
+// Hola Francesc
+// pelo Firefox
+// Javier Guerra18:07
+// Ya lo tienes registrado en Dog CEP API ?
+// Tú18:07
+// se come la BD
+// más criptonita tenías que echarle
+// Javier Guerra18:09
+// localstorage?
+// Javier Guerra18:13
+// yupi!
+// Javier Guerra18:15
+// ir rotando
+// Sergio Alvariño18:16
+// Buenas tardes, perdón por tardar (mi PC, que es lentísimo)
+// Héctor Fdez. Josa18:18
+// Me tengo que ir, ciao
+// Javier Guerra18:24
+// Sí, si estoy de acuerdo Marina.
+// Tú18:25
+// yo hice para mi clase una herramienta para hacer grupitos aleatorizados de estudiantes: https://github.com/V3RNE42/cpp_projects/blob/main/ClasePrimeroDAM2.cpp
+// no problem
+// ok
+// Marina Vallejo18:26
+// castigados
+// Tú18:26
+// yo retoco entonces
+// gracias por incluírme entre los guais
+// Marina Vallejo18:26
+// :____
+// Tú18:27
+// Marina, tú no eres guay
+// Javier Guerra18:27
+// Julio, tu eres guay porque siempre tienes pa comer
+// Marina Vallejo18:27
+// stop bullying
+// Tú18:29
+// me siento identificado con tu perro7
+// Marina Vallejo18:30
+// alejandro me comenta que está malo con fiebre que está en consulta
+// no cree que se conecte ya en toda la tarde
+// Tú18:35
+// Aquí Marina https://meet.google.com/utx-syqg-pcu
+// Javier Guerra18:36
+// Luego comento dos dudas que me han surgido.
+// Anatoli18:36
+// Borja: https://meet.google.com/xuc-osni-ncr
+// Javier Guerra18:37
+// Sergio: https://meet.google.com/pcy-msyj-oeb
+// Alex Castu18:37
+// Jesus : https://meet.google.com/obd-pysr-yns?authuser=2
+// Alex Castu18:45
+// Denis te vienes un seg?
+// Denis Jiménez álvaro19:00
+// Chicos, en general, cómo vais? 
+// Marina Vallejo19:01
+// bastante bien, me estoy enterando de casi todo :p
+// Anatoli19:01
+// Bien, bien
+// Juan José Casco Martín-Javato19:02
+// Genial!
+// Denis Jiménez álvaro19:02
+// Pero qué nos queda ... Más o menos? :S 
+// Marina Vallejo19:13
+// lo tenemos hecho ya, pero estoy preguntando todo lo que no tengo claro
+// Juan José Casco Martín-Javato19:14
+// olee
+// Genial marina
+// Denis Jiménez álvaro19:19
+// ¿QUIÉN QUEDA POR TERMINAR?
+// Alex Castu19:20
+// nosotros
+// Tú19:20
+// nosotros ya estamos
+// Denis Jiménez álvaro19:21
+// En poco corregimos, chicos
+// Javier Guerra19:25
+// https://dog.ceo/dog-api/
+// Alex Castu19:32
+// no
+// Javier Guerra19:33
+// Aquí estamos
+// Marina Vallejo19:36
+// ok
+// jesus pascual19:36
+// vale
+// Denis Jiménez álvaro19:37
+// No obstante, como siempre, TODO el que quiera compartir su solución por Slack será bienvenido (os animo) 
+// Marina Vallejo20:25
+// linea 33, \r
+// ????
+// Tú20:27
+// spread
+// https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Operators/Spread_syntax
+// Marina Vallejo20:27
+// gracias!
+// Marina Vallejo20:31
+// el onload y onerror?
+// 276 y 279
+// Juan José Casco Martín-Javato20:33
+// Marina https://javascript.info/onload-onerror
+// Te lo mando asi porque yo tampoco lo habia visto
+// Marina Vallejo20:44
+// gracias!
+// don javier, comparta usted su código, porfavore
+// Javier Guerra20:45
+// A lo mejor compartir pantalla se puede...
+// pero no le vemos a él
+// Javier Guerra20:58
+// Mu bien Marina. poco a poco.
+// Denis Jiménez álvaro20:58
+// Sí, muy bien Marina
+// Borja Toledo20:58
+// Jesuslin de Ubrisque
+// Marina Vallejo20:58
+// gracias! yo es que iba en plan preguntando por qué se ponen dos then en vez de uno XD Pero de lo que he hecho me he enterado, ya es un progreso!!
+// Javier Guerra20:59
+// :+1:
+// Tú21:05
+// este último ejercicio dónde está puesto (??)
+// Tú21:06
+// no lo veo en el canal de enunciados
+// Juan José Casco Martín-Javato21:06
+// https://github.com/TheBridge
+// Juan José Casco Martín-Javato21:08
+// Julio: https://github.com/TheBridge-FullStackDeveloper/fullstack_INDRA_PT_mar22/blob/main/teoria/fundamentos-javascript/clase5.md
+// Abajo del todo
+// Tú21:08
+// gracias guapo
+// :-*
+// Javier Guerra21:08
+// Hazlo im-prezionante
+// Juan José Casco Martín-Javato21:08
+// :D
+// Marina Vallejo21:11
+// qué diferencia hay entre +raza+ y lo que tenía yo que era así ${} ???
+// Juan José Casco Martín-Javato21:11
+// que +raza+ es mas feo
+// Utiliza siempre ${}
+// Marina Vallejo21:11
+// okis
+// si hubiera puesto ${} en su código funcionaría exactamente igual??
+// Javier Guerra21:12
+// Falta la de su perro
+// Juan José Casco Martín-Javato21:12
+// siempre y cuando el la cadena empiece y acabe con ` ` 
+// Marina Vallejo21:12
+// merci
+// Juan José Casco Martín-Javato21:12
+// Nada
+// Marina Vallejo21:13
+// Muy bien expuesto, Jesús! :D
+// Juan José Casco Martín-Javato21:14
+// fetch(...)
+//    .then(response => response.json())
+//    .then(data=> console.log(data.message)
+// Asi seria lo que dijo Denis
